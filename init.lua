b13a68bbd062768a61c0f70711bc0e776ac935b8
yaeju1205 <gimleni593@gmail.com>
2026-03-26 22:09:06 +0900

    feat: Add gitsigns.nvim

--- init.lua
+++ init.lua
@@ -436,6 +436,26 @@ plugins.install("yaeju1205/sakura.nvim", {
     cmd("colorscheme sakura")
 end)
 
+plugins.install("lewis6991/gitsigns.nvim")(function()
+    require("gitsigns").setup({
+        signs = {
+            add = { text = "┃" },
+            change = { text = "┃" },
+            delete = { text = "┃" },
+            topdelete = { text = "┃" },
+            changedelete = { text = "┃" },
+            untracked = { text = "┆" },
+        },
+        signs_staged = {
+            add = { text = "┃" },
+            change = { text = "┃" },
+            delete = { text = "┃" },
+            topdelete = { text = "┃" },
+            changedelete = { text = "┃" },
+            untracked = { text = "┆" },
+        },
+    })
+end)
 plugins.install("lewis6991/satellite.nvim")(function()
     require("satellite").setup({
         current_only = false,
